f3e5ee2f7118cbd8cb0ab3010ed827d1e4f5eddf
Hacker <Alexmeline1@gmail.com>
2026-05-11 13:05:06 -0400

    Updating to default commands

--- src/main/java/frc/subsystems/driveSubsystem.java
+++ src/main/java/frc/subsystems/driveSubsystem.java
@@ -7,16 +7,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
 public class driveSubsystem extends SubsystemBase{
-    private final PWMVictorSPX m_leftMotor = new PWMVictorSPX(RobotMap.LeftMotorPWMPort);
-    private final PWMVictorSPX m_rightMotor = new PWMVictorSPX(RobotMap.RightMotorPWMPort);
-
-    public void LeftTrackControl(Supplier<Double>Speed){
-    m_leftMotor.set(Speed.get());
-    }
-    public void RightTrackControl(Supplier<Double>Speed){
-    m_rightMotor.set(Speed.get());
-    }
-
+    private final static PWMVictorSPX m_leftMotor = new PWMVictorSPX(RobotMap.LeftMotorPWMPort);
+        private final static PWMVictorSPX m_rightMotor = new PWMVictorSPX(RobotMap.RightMotorPWMPort);
 }
-
-
+    
+    
+public setDefaultCommand LeftTrackControl(Supplier<Double>Speed){
+    m_leftMotor.set(Speed.get());
+}
+public setDefaultCommand RightTrackControl(Supplier<Double>Speed){
+    m_rightMotor.set(Speed.get());
+}
